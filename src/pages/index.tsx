ab74baf07ba36c29e5f8583200c592f31c77399c
LzMiguel <miguel.gandra10@gmail.com>
2023-02-21 19:07:27 -0300

    459-aula
    até a  aula 459

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import Layout from "../components/Layout";
 import React from "react";
 
 export default function Home() {
     return <div className={`flex justify-center items-center h-screen 
     bg-gradient-to-r from-blue-500 to-purple-500
     text-white`}>
-      <h1>App</h1>
+      <Layout titulo="Cadastro Simples">
+        <span>Cadastro</span>
+      </Layout>
     </div>;
 }
