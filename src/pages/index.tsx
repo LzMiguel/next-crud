f0cfe7e6463c28152272441512151da682d882c4
LzMiguel <miguel.gandra10@gmail.com>
2023-02-22 17:08:18 -0300

    to-464
    Até a aula 464

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,43 @@
 import Layout from "../components/Layout";
 import React from "react";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
+import Formulario from "../components/Formulario";
+import Botao from "../components/Botao";
 
 export default function Home() {
-    return <div className={`flex justify-center items-center h-screen 
-    bg-gradient-to-r from-blue-500 to-purple-500
-    text-white`}>
-      <Layout titulo="Cadastro Simples">
-        <span>Cadastro</span>
-      </Layout>
-    </div>;
+    const clientes = [
+        new Cliente("Ana", 22, "1"),
+        new Cliente("Jhonny", 25, "2"),
+        new Cliente("Frank", 33, "3"),
+        new Cliente("Bean", 41, "4"),
+        new Cliente("Tobi", 15, "5"),
+        new Cliente("Kakashi", 36, "6"),
+    ];
+    function clienteSelecionado(cliente: Cliente) {
+        console.log(cliente.nome);
+    }
+    function clienteExcluido(cliente: Cliente) {
+        console.log(cliente.nome);
+    }
+    return (
+        <div
+            className={`
+                    flex justify-center items-center h-screen 
+                    bg-gradient-to-r from-blue-500 to-purple-500
+                    text-white`}
+        >
+            <Layout titulo="Cadastro Simples">
+                <div className="flex justify-end">
+                    <Botao className="mb-4" cor="green">Novo Cliente</Botao>
+                </div>
+                {/* <Tabela
+                    clientes={clientes}
+                    clienteSelecionado={clienteSelecionado}
+                    clienteExcluido={clienteExcluido}
+                /> */}
+                <Formulario />
+            </Layout>
+        </div>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    purge:{
     content: [
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
+    safelist:[
+        /^bg-/,
+        /^to-/,
+        /^from-/,
+    ]
+},
     theme: {
         extend: {},
     },
